e809723536f2ffdd55e85ec5e204f85fae2d47a1
Bhyun <superay190@gmail.com>
2024-03-23 12:06:59 -0400

    Base Website Finished

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+//for static method
+const static_files_router = express.static('static')
+app.use( static_files_router )
+
+// for post method
+const bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Home route
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
